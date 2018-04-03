58f971f59904932eca787c386fbf4a94b1946c47
Heshmatkhah <maheshmatkhah.prg@gmail.com>
2018-04-03 17:11:16 +0430

    update license and tests

--- global_login_required/views.py
+++ global_login_required/views.py
@@ -1,6 +1,4 @@
-from django.utils.decorators import method_decorator
-
-from django.views.generic import TemplateView
+from django.views import View
 from django.http import HttpResponse
 from . import login_not_required
 
@@ -9,8 +7,9 @@ def test_FunctionBasedView(request, **kwargs):
 	return HttpResponse("Response from view.")
 
 
-class test_ClassBasedView(TemplateView):
-	template_name = 'test.html'
+class test_ClassBasedView(View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("Response from view.")
 
 
 @login_not_required
@@ -18,5 +17,6 @@ def test_FunctionBasedView_decorator(request, **kwargs):
 	return HttpResponse("Response from view.")
 
 
-class test_ClassBasedView_decorator(TemplateView):
-	template_name = 'test.html'
+class test_ClassBasedView_decorator(View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("Response from view.")
